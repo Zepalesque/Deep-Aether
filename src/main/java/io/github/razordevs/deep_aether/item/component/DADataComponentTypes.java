ef05aa3987acab6bb754b8769e0b092ad49c4c5c
darksonic300 <fabiross096@gmail.com>
2024-10-26 11:57:33 +0200

    port: I f*cked up

--- src/main/java/io/github/razordevs/deep_aether/item/component/DADataComponentTypes.java
+++ src/main/java/io/github/razordevs/deep_aether/item/component/DADataComponentTypes.java
@@ -2,14 +2,13 @@ package io.github.razordevs.deep_aether.item.component;
 
 import io.github.razordevs.deep_aether.DeepAether;
 import net.minecraft.core.component.DataComponentType;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class DADataComponentTypes {
     public static final DeferredRegister.DataComponents DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(
-            ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(DeepAether.MODID, "data_components")), DeepAether.MODID);
+            Registries.DATA_COMPONENT_TYPE, DeepAether.MODID);
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<DungeonTracker>> DUNGEON_TRACKER = DATA_COMPONENT_TYPES.registerComponentType(
             "dungeon_tracker",
