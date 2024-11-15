368f082a5322a3ff61562d3ba1d5cec82a7d2dad
TunefulTurnip <tunefulturnip@gmail.com>
2024-11-15 16:25:50 +0100

    Updated Eots model

--- src/main/java/io/github/razordevs/deep_aether/client/model/EOTSSegmentModel.java
+++ src/main/java/io/github/razordevs/deep_aether/client/model/EOTSSegmentModel.java
@@ -15,7 +15,6 @@ import net.neoforged.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class EOTSSegmentModel extends HierarchicalModel<EOTSSegment> {
     private final ModelPart body;
-    private final ModelPart head;
     private final ModelPart bb_main;
     private final ModelPart bb_segment;
     private final ModelPart upperMouth;
@@ -23,11 +22,10 @@ public class EOTSSegmentModel extends HierarchicalModel<EOTSSegment> {
 
     public EOTSSegmentModel(ModelPart root) {
         this.body = root.getChild("body");
-        this.head = root.getChild("head");
         this.bb_main = root.getChild("bb_main");
         this.bb_segment = root.getChild("bb_segment");
-        this.upperMouth = head.getChild("upperMouth");
-        this.lowerMouth = head.getChild("lowerMouth");
+        this.upperMouth = bb_main.getChild("Head").getChild("upperMouth");
+        this.lowerMouth = bb_main.getChild("Head").getChild("lowerMouth");
     }
 
     public static LayerDefinition createBodyLayer() {
@@ -37,20 +35,37 @@ public class EOTSSegmentModel extends HierarchicalModel<EOTSSegment> {
         partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -8.0F, 0.0F, 14.0F, 12.0F, 16.0F,
                 new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 18.0F, -8.0F));
 
-        partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(26, 41).addBox(-8.0F, -13.0F, 0.0F, 3.0F, 10.0F, 10.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 38).addBox(5.0F, -13.0F, 0.0F, 3.0F, 10.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
-
         partdefinition.addOrReplaceChild("bb_segment", CubeListBuilder.create().texOffs(28, 28).addBox(-9.0F, -13.0F, -6.0F, 2.0F, 10.0F, 12.0F, new CubeDeformation(0.0F))
                 .texOffs(0, 28).addBox(7.0F, -13.0F, -6.0F, 2.0F, 10.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition head =  partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-3.5F, -9.0F, -10.0F, 11.0F, 13.0F, 13.0F, new CubeDeformation(0.0F))
-                .texOffs(42, 30).addBox(2.0F, -13.0F, -9.5F, 0.0F, 4.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 18.0F, 8.0F));
 
-        head.addOrReplaceChild("upperMouth", CubeListBuilder.create().texOffs(0, 26).addBox(-5.0F, -3.0F, -9.0F, 10.0F, 3.0F, 9.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(1.0F, -5.0F, -7.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 4).addBox(-3.0F, -5.0F, -7.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 1.0F, -10.0F));
+        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create(), PartPose.offset(0.0F, 22.0F, 0.0F));
 
-        head.addOrReplaceChild("lowerMouth", CubeListBuilder.create().texOffs(29, 29).addBox(-3.0F, 0.0F, -9.0F, 10.0F, 3.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -10.0F));
+        bb_main.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(44, 13).addBox(-4.0F, -5.0F, 0.0F, 8.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -7.0F, -2.0F, 0.4363F, 0.0F, 0.0F));
+
+        PartDefinition Head11 = bb_main.addOrReplaceChild("Head11", CubeListBuilder.create(), PartPose.offset(2.0F, -6.0F, 8.0F));
+
+        Head11.addOrReplaceChild("Head_r1", CubeListBuilder.create().texOffs(16, 28).addBox(-3.5F, -1.0F, -4.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.0F, -6.0F, 1.0F, 1.0462F, 0.5295F, 0.2342F));
+
+        PartDefinition Head10 = bb_main.addOrReplaceChild("Head10", CubeListBuilder.create(), PartPose.offset(-2.0F, -6.0F, 8.0F));
+
+        Head10.addOrReplaceChild("Head_r2", CubeListBuilder.create().texOffs(0, 21).addBox(1.5F, -6.0F, -4.0F, 2.0F, 7.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.0F, 0.0F, -2.0F, 0.2618F, -0.3054F, 0.0F));
+
+        PartDefinition Head2 = bb_main.addOrReplaceChild("Head2", CubeListBuilder.create(), PartPose.offset(2.0F, -6.0F, 8.0F));
+
+        Head2.addOrReplaceChild("Head_r3", CubeListBuilder.create().texOffs(0, 40).addBox(-3.5F, -1.0F, -4.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.0F, -3.0F, 3.0F, 0.5662F, 0.5295F, 0.2342F));
+
+        Head2.addOrReplaceChild("Head_r4", CubeListBuilder.create().texOffs(28, 9).addBox(-3.5F, -6.0F, -4.0F, 2.0F, 7.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.0F, 0.0F, -2.0F, 0.2618F, 0.3054F, 0.0F));
+
+        PartDefinition Head = bb_main.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -4.0F, -10.0F, 8.0F, 9.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, -6.0F, 8.0F));
+
+        Head.addOrReplaceChild("Head_r5", CubeListBuilder.create().texOffs(32, 30).addBox(1.5F, -1.0F, -4.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.0F, -3.0F, 3.0F, 0.5662F, -0.5295F, -0.2342F));
+
+        Head.addOrReplaceChild("Head_r6", CubeListBuilder.create().texOffs(16, 42).addBox(1.5F, -1.0F, -4.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.0F, -6.0F, 1.0F, 1.0462F, -0.5295F, -0.2342F));
+
+        Head.addOrReplaceChild("upperMouth", CubeListBuilder.create().texOffs(44, 0).addBox(-2.5F, -3.0F, -8.0F, 5.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 3.0F, -10.0F));
+
+        Head.addOrReplaceChild("lowerMouth", CubeListBuilder.create().texOffs(70, 3).addBox(-2.5F, 0.0F, -8.0F, 5.0F, 2.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 3.0F, -10.0F));
 
         return LayerDefinition.create(meshdefinition, 128, 128);
     }
@@ -62,9 +77,8 @@ public class EOTSSegmentModel extends HierarchicalModel<EOTSSegment> {
         if(entity.isControllingSegment()) {
             body.visible = false;
             bb_segment.visible = false;
-            head.visible = true;
             bb_main.visible = true;
-            upperMouth.visible = true;
+
             if(entity.isMouthOpen()) {
                 if(upperMouth.xRot > -0.3F) {
                     float mouthRotation = Mth.lerp(ageInTicks * 0.01F,  upperMouth.xRot, -0.3F);
@@ -80,16 +94,13 @@ public class EOTSSegmentModel extends HierarchicalModel<EOTSSegment> {
         } else {
             body.visible = true;
             bb_segment.visible = true;
-            head.visible = false;
             bb_main.visible = false;
-            upperMouth.visible = false;
         }
     }
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, int color) {
         body.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-        head.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
         bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
         bb_segment.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
     }
